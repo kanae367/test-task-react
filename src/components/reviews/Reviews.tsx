7faaa5b83162d10531ee6467433316044a71df4c
”kodokunawa” <kodokunawa@gmail.com>
2024-06-03 23:30:26 +0300

    Реализован компонент reviews

--- src/components/reviews/Reviews.tsx
+++ src/components/reviews/Reviews.tsx
@@ -3,7 +3,37 @@ import './reviews.scss';
 function Reviews(){
     return(
         <section className='reviews'>
-            
+            <h2 className='reviews__title'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
+            <div className='reviews__container'>
+                <div className='reviews__item'>
+                    <img className='reviews__item-image' src="/reviews/photo.png" alt="Фото автора" />
+                    <div className='reviews__item-content'>
+                        <p className='reviews__item-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quas, aut consectetur
+ animi  autem aliquid consequuntur suscipit exercitationem laborum cupiditate magnam eaque quae deleniti, iste nisi
+ expedita, provident excepturi officia!
+ Porro ab rerum omnis magnam eligendi error nobis dolore? Porro ab rerum omnis magnam eligendi error nobis dolore? Porro ab rerum omnis magnam eligendi error nobis dolore?</p>
+                        <span className='reviews__item-author'>Jana Doe</span>
+                    </div>
+                </div>
+                
+                <div className='reviews__item'>
+                    <img className='reviews__item-image' src="/reviews/photo.png" alt="Фото автора" />
+                    <div className='reviews__item-content'>
+                        <p className='reviews__item-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quas, aut consectetur
+ animi  autem aliquid</p>
+                        <span className='reviews__item-author'>Jana Doe</span>
+                    </div>
+                </div>
+
+                <div className='reviews__item'>
+                    <img className='reviews__item-image' src="/reviews/photo.png" alt="Фото автора" />
+                    <div className='reviews__item-content'>
+                        <p className='reviews__item-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quas, aut consectetur
+ animi  autem aliquid</p>
+                        <span className='reviews__item-author'>Jana Doe</span>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
